0ab297eb15c86c069d63f07a51b4d87fbc7247a8
Lee Lazarecky <leelazarecky@gmail.com>
2020-07-31 22:08:04 -0400

    Creates two define statements to clarify usage of API and testing functions.

--- src/lib/chip8_emu.h
+++ src/lib/chip8_emu.h
@@ -10,41 +10,30 @@
 
 using namespace std;
 
+// This is supposed to do nothing. It is indended for developers to understand what the public API is.
+#define EMU_API /**/
+#define EMU_PUBLIC_FOR_TESTING_PURPOSES public
+
 class Chip8Emu {
 public:
-    Chip8Emu();
+    EMU_API Chip8Emu();
 
-    static const size_t VFX_WIDTH = 64;
-    static const size_t VFX_HEIGHT = 32;
+    EMU_API void load(const std::string &game_path);
 
-    void fetch_op_code();
-
-    void load(const std::string &game_path);
-
-    void run();
-
-    enum KeyState : uint16_t {
-        RELEASED = 0,
-        PRESSED,
-    };
+    EMU_API void run();
 
 public:
-    using InputKeyBuffer = std::array<uint8_t, 16>;
-
-    // Callbacks given to frontend applications
-public:
-
     // - [NOT INHERENTLY THREAD SAFE, YOU MUST GUARD THIS FUNCTION CALLS]
     // - [POINTER IS ALWAYS VALID]
-    std::function<bool(const uint8_t *vfx, size_t size)> render_video_frame_cb;
+    EMU_API std::function<bool(const uint8_t *vfx, size_t size)> render_video_frame_cb;
 
     // [NOT INHERENTLY THREAD SAFE, YOU MUST GUARD THIS FUNCTION CALLS]
-    std::function<bool(void)> play_audio_cb;
+    EMU_API std::function<bool(void)> play_audio_cb;
 
     // - [NOT INHERENTLY THREAD SAFE, YOU MUST GUARD THIS FUNCTION CALLS]
     // ARG_1 - The index of the input buffer 0-15.
     // RETURN_TYPE - The state of the input key passed in as an arg. 0 or 1
-    std::function<uint8_t (size_t input_key)> update_input_key_state_cb;
+    EMU_API std::function<uint8_t(size_t input_key)> update_input_key_state_cb;
 
 private:
 
@@ -56,15 +45,24 @@ private:
 
     void inc_instruction();
 
-
-
     static int extract_x_bit(uint16_t op_code);
 
     static int extract_y_bit(uint16_t op_code);
 
 
+EMU_PUBLIC_FOR_TESTING_PURPOSES:
+    using InputKeyBuffer = std::array<uint8_t, 16>;
 
-public:
+    enum KeyState : uint16_t {
+        RELEASED = 0,
+        PRESSED,
+    };
+
+    static const size_t VFX_WIDTH = 64;
+    static const size_t VFX_HEIGHT = 32;
+
+    void fetch_op_code();
+
     std::array<uint8_t, 4096> memory{0x0};
     uint16_t pc{0x200};
     uint16_t i_register{0};
@@ -82,7 +80,6 @@ public:
 
     std::vector<uint8_t> game_buffer;
 
-public:
     bool game_loaded_flag{false};
 
 };
